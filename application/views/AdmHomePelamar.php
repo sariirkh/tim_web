3acfed13a787ff1e7d1c29d162cf26c43b597522
Azizah789 <47998993+Azizah789@users.noreply.github.com>
2020-11-20 10:22:51 +0700

    update lagi

--- application/views/AdmHomePelamar.php
+++ application/views/AdmHomePelamar.php
@@ -22,51 +22,50 @@
       <div class="container-fluid">
         <!-- Info boxes -->
         <div class="row">
-          <div class="col-1 col-sm-6 col-md-4">
-            <div class="info-box mb-3">
-             <span class="info-box-icon bg-success elevation-1"><i class="fas fa-arrow-circle-left"></i></span>
+          <div class="col-12 col-sm-6 col-md-4">
+          <div class="small-box bg-success">
+              <div class="inner">
+              <h3><?php echo $totOpen; ?></h3>
 
-              <div class="info-box-content">
-                <span class="info-box-text">Pelamar Open</span>
-                <span class="info-box-number">
-                <a href="Pelamar">  <?php echo $totOpen; ?></a>
-                </span>
+                <p>Pelamar Open</p>
               </div>
-              <!-- /.info-box-content -->
+              <div class="icon">
+                <i class="ion ion-android-person-add"></i>
+              </div>
+              <a href="Pelamar" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
-            <!-- /.info-box -->
           </div>
           <!-- /.col -->
           <div class="col-12 col-sm-6 col-md-4">
-            <div class="info-box mb-3">
-              <span class="info-box-icon bg-info elevation-1"><i class="fas fa-users"></i></span>
+          <div class="small-box bg-info">
+              <div class="inner">
+                <h3><?php echo $totDiterima; ?></h3>
 
-              <div class="info-box-content">
-                <span class="info-box-text">Pelamar Diterima</span>
-                <span class="info-box-number">
-                <a href="Pelamar">  <?php echo $totDiterima; ?></a>
+                <p>Pelamar Diterima</p>
               </div>
-              <!-- /.info-box-content -->
+              <div class="icon">
+                <i class="ion ion-checkmark-circled"></i>
+              </div>
+              <a href="Pelamar" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
-            <!-- /.info-box -->
-          </div>
+            </div>
           <!-- /.col -->
 
           <!-- fix for small devices only -->
           <div class="clearfix hidden-md-up"></div>
 
           <div class="col-12 col-sm-6 col-md-4">
-            <div class="info-box mb-3">
-              <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-users"></i></span>
+          <div class="small-box bg-danger">
+              <div class="inner">
+                <h3><?php echo $totGagal; ?></h3>
 
-              <div class="info-box-content">
-                <span class="info-box-text">Pelamar Gagal</span>
-                <span class="info-box-number">
-                <a href="Pelamar">  <?php echo $totGagal; ?></a>
+                <p>Pelamar Gagal</p>
               </div>
-              <!-- /.info-box-content -->
+              <div class="icon">
+                <i class="ion ion-android-cancel"></i>
+              </div>
+              <a href="Pelamar" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
-            <!-- /.info-box -->
           </div>
           <!-- /.col -->
           <!-- <div class="col-12 col-sm-6 col-md-3">
@@ -86,37 +85,94 @@
         <!-- /.row -->
         <!-- </div> -->
           <!-- /.col-md-6 -->
-          <div class="container-fluid">
-        <!-- Small boxes (Stat box) -->
-        <div class="row">
-        <div class="col-6">
-      <div class="card">
+          <!-- PIE CHART -->
+         
+           <!-- Small boxes (Stat box) -->
+        <div class="col-4">
+          <div class="card card-danger">
               <div class="card-header">
-                <h3 class="card-title">Absence Diagram</h3>
+                <h3 class="card-title">Gender Diagram</h3>
+
                 <div class="card-tools">
-                  <a href="javascript:void(0);">View Report</a>
+                  <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i>
+                  </button>
+                  <button type="button" class="btn btn-tool" data-card-widget="remove"><i class="fas fa-times"></i></button>
+                </div>
+              </div>
+              <div class="card-body">
+                <canvas id="pieChartJenisKelamin" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+                  </span>
+                  </div>
+              
+             <!-- PIE CHART -->
+             <div class="col-4">
+          <div class="card card-warning">
+              <div class="card-header">
+                <h3 class="card-title">Graduates Diagram</h3>
+
+                <div class="card-tools">
+                  <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i>
+                  </button>
+                  <button type="button" class="btn btn-tool" data-card-widget="remove"><i class="fas fa-times"></i></button>
+                </div>
+              </div>
+              <div class="card-body">
+                <canvas id="pieChartLulusan" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+      </div>
+            <!-- /.card -->
+            <!-- Small boxes (Stat box) -->
+        <div class="col-4">
+          <div class="card card-success">
+              <div class="card-header">
+                <h3 class="card-title">City Diagram</h3>
+
+                <div class="card-tools">
+                  <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i>
+                  </button>
+                  <button type="button" class="btn btn-tool" data-card-widget="remove"><i class="fas fa-times"></i></button>
+                </div>
+              </div>
+              <div class="card-body">
+                <canvas id="pieChartKota" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+                  
+            </span>
+                  </div>
+              
+             <!-- BAR CHART -->
+             <!-- Small boxes (Stat box) -->
+             <div class="col-12">
+          <div class="card card-info">
+              <div class="card-header border-3">
+              <div class="d-flex justify-content-between">
+                <h3 class="card-title">Data Pelamar Dalam 1 Tahun</h3>
+                <a href="javascript:void(0);">View Report</a>
                 </div>
               </div>
               <div class="card-body">
                 <div class="d-flex">
-                  <p class="d-flex flex-column">
-                    <span class="text-bold text-lg">$18,230.00</span>
-                    <span>Sales Over Time</span>
-                  </p>
                   <p class="ml-auto d-flex flex-column text-right">
-                    <span class="text-success">
-                      <i class="fas fa-arrow-up"></i> 33.1%
-                    </span>
-                    <span class="text-muted">Since last month</span>
+                  <span class="text-muted">Since last month</span>
                   </p>
                 </div>
                 <!-- /.d-flex -->
 
                 <div class="position-relative mb-4">
-                  <canvas id="sales-chart" height="200"></canvas>
+                  <canvas id="barChart" height="100"></canvas>
                 </div>
 
-                <div class="d-flex flex-row justify-content-end">
+             <div class="d-flex flex-row justify-content-end">
                   <span class="mr-2">
                     <i class="fas fa-square text-primary"></i> This year
                   </span>
@@ -129,75 +185,8 @@
             </div>
             </div>
             <!-- /.card -->
-    
-        <!-- Small boxes (Stat box) -->
-        <div class="col-6">
-      <div class="card">
-              <div class="card-header">
-                <h3 class="card-title">Absence Diagram</h3>
-                <div class="card-tools">
-                  <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i>
-                  </button>
-                  <button type="button" class="btn btn-tool" data-card-widget="remove"><i class="fas fa-times"></i>
-                  </button>
-                </div>
-              </div>
-              <!-- /.card-header -->
-              <div class="card-body">
-                <div class="row">
-                  <div class="col-md-8">
-                    <div class="chart-responsive">
-                      <canvas id="pieChart" height="195"></canvas>
-                    </div>
-                    <!-- ./chart-responsive -->
-                  </div>
-                  <!-- /.col -->
-                  <div class="col-md-4">
-                    <ul class="chart-legend clearfix">
-                      <li><i class="far fa-circle text-danger"></i> Chrome</li>
-                      <li><i class="far fa-circle text-success"></i> IE</li>
-                      <li><i class="far fa-circle text-warning"></i> FireFox</li>
-                      <li><i class="far fa-circle text-info"></i> Safari</li>
-                      <li><i class="far fa-circle text-primary"></i> Opera</li>
-                      <li><i class="far fa-circle text-secondary"></i> Navigator</li>
-                    </ul>
-                  </div>
-                  <!-- /.col -->
-                </div>
-                <!-- /.row -->
-              </div>
-              <!-- /.card-body -->
-              <div class="card-footer bg-white p-0">
-                <ul class="nav nav-pills flex-column">
-                  <li class="nav-item">
-                    <a href="#" class="nav-link">
-                      United States of America
-                      <span class="float-right text-danger">
-                        <i class="fas fa-arrow-down text-sm"></i>
-                        12%</span>
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a href="#" class="nav-link">
-                      India
-                      <span class="float-right text-success">
-                        <i class="fas fa-arrow-up text-sm"></i> 4%
-                      </span>
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a href="#" class="nav-link">
-                      China
-                      <span class="float-right text-warning">
-                        <i class="fas fa-arrow-left text-sm"></i> 0%
-                      </span>
-                    </a>
-                  </li>
-                </ul>
-              </div>
-              <!-- /.footer -->
-            </div>
-      </div>
-            <!-- /.card -->
+
+      
+            </div><!--/. container-fluid -->
         </section>
         </div>
